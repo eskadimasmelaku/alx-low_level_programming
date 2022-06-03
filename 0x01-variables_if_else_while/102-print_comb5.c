a2089bb1705bdccec31904e0eb89e4cd9ce681b6
Solomon <solomonaz2022@gmail.com>
2022-06-03 15:43:18 -0700

    102 last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -33,7 +33,7 @@ int main(void)
 						putchar('0' + c);
 						if (!(f + e == 18 && c + d == 17 && d == 9))
 						{
-							putchar(',')
+							putchar(',');
 							putchar(' ');
 						}
 					}
