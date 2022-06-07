749cf5e2c8d2610e4a1d3dc0aff32a62415c9c51
Solomon <solomonaz2022@gmail.com>
2022-06-07 14:55:23 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,19 +15,12 @@ void print_times_table(int n)
 	int i;
 	int j;
 
-
 	if (n >= 0 && n <= 15)
 	{
-
-	
 		for (i = 0; i <= n; i++)
 		{
-
-		
 			for (j = 0; j <= n; j++)
 			{
-
-			
 				if ((i * j) < 10)
 				{
 					if (j != 0)
@@ -38,8 +31,6 @@ void print_times_table(int n)
 					}
 					_putchar((j * i) + '0');
 				}
-
-			
 				else if ((i * j) < 100)
 				{
 					_putchar(' ');
@@ -47,8 +38,6 @@ void print_times_table(int n)
 					_putchar((((j * i) / 10) % 10) + '0');
 					_putchar(((j * i) % 10) + '0');
 				}
-
-			
 				else if ((i * j) < 1000)
 				{
 					_putchar(' ');
@@ -60,17 +49,8 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 				}
-
-			
 			}
-
-		
 			_putchar('\n');
-
-		
 		}
-
-	
 	}
-
 }
