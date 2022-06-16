fa618bda939241d89abe03801ea46b858e6ac7b7
Solomon <solomonaz2022@gmail.com>
2022-06-16 00:59:47 -0700

    String capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,42 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *  * *cap_string - this is awesome
- *   * @s: pointer to char params
- *    *
- *     * Return: *s
+ *  * *cap_string - capitalize words
+ *   * @str: pointer
+ *    * Return: capitalzied string
  */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int i, j;
-	char delimeters[] = " \t\n,;.!?\"(){}";
+	char sep[] = ",\t;\n; .!?\"(){}";
+	int flag, i, ii;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = s[0] - 32;
-				for (j = 0; delimeters[j] != '\0'; j++)
-					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)	
-						s[i + 1] = s[i + 1] - 32;				
-	}
+		flag = 0;
+		if (i == 0)
+		{
+			flag = 1;
+		}
+		else
+		{
+			for (ii = 0; sep[ii] != '\0'; ii++)
+			{
+				if (str[i - 1] == sep[ii])
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
 
-	return (s);
+		if (flag == 1)
+		{
+			if (str[i] <= 'z' && str[i] >= 'a')
+			{
+				str[i] -= ('a' - 'A');
+			}
+		}
+	}
+	return (str);
 }
