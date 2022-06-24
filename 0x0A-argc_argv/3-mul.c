7b4a5efa15b45237b19d496c8145f7bf93098131
Solomon <solomonaz2022@gmail.com>
2022-06-24 06:26:51 -0700

    Arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,8 @@ int main(int argc, char const *argv[])
 
 	if (argc != 3)
 	{
-			printf("Error\n");
-				return (1);
+		printf("Error\n");
+		return (1);
 	}
 	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
