f0411869bf9133ecf837923ae097f9dd62ef0ea4
Solomon <solomonaz2022@gmail.com>
2022-06-30 11:10:08 -0700

    Malloc and free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,8 +12,10 @@ int main(int argc, char *argv[])
 	int i, j;
 
 	if (argc != 3)
-	{ printf("Error\n");
-		exit(98); }
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
