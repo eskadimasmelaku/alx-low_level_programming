03b2175ae0dfe3447573b74813a294eb49a2d244
Eskadimas <eskadimasmelaku@gmail.com>
2023-02-17 14:22:57 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,23 +7,24 @@
 
 int main(void)
 {
-        char c;
+	char c;
 
 	char d;
 
-        c = 'a';
+	c = 'a';
 	d = 'A';
 	while
 		(c <= 'z') {
-                        putchar(c);
-                        c++;
-                }
-	while 
+			putchar(c);
+			c++;
+		}
+
+	while
 		(d <= 'Z') {
 			putchar(d);
 			d++;
 		}
-        putchar('\n');
+	putchar('\n');
 	return (0);
-}
 
+}
