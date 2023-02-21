84306df6db9fb57d7cd0429dd2acef13d867d2bc
Eskadimas <eskadimasmelaku@gmail.com>
2023-02-21 05:04:20 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+
+	return (1);
+	}
+	return (0);
+}
+
