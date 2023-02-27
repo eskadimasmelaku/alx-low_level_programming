0367364de1f65d13fa8eedada39ab8df834b456c
Eskadimas <eskadimasmelaku@gmail.com>
2023-02-27 05:00:52 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - print every other charcater
+ * @str: the string to be targated
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int j;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	for  (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
