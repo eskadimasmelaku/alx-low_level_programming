a7e0b962d91885e4c659d58d3ef7ca21400576e7
Eskadimas <eskadimasmelaku@gmail.com>
2023-03-02 02:26:39 -0800

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - reverses the content of an array of integers
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
